9afdfb549b29b68c5d6ab483b270196852fc8747
thiagoyellow <thiago.g.santos@outlook.com.br>
2020-06-07 19:58:36 -0300

    Aplicação Ecoleta (NLW RocketSeat)

---
+++ public/scripts/intro.js
@@ -0,0 +1,61 @@
+//comments
+//document.write("Hello")
+
+//variáveis, tipos de dados (string, number, boolean)
+
+
+// todos os objetos possuem propriedades e funcionalidades
+
+ 
+
+  //const pessoa = {
+ // idade: 24,
+//  altura: "1,75m",
+//  nome: "Thiago",
+//}
+  
+//document.write(pessoa.nome) 
+
+
+
+//Array = Vetores
+
+//const colecao = ["capim", "mato", "charuto", {name: "Thiago" }]
+
+ //document.write(colecao[3].name)
+
+
+//Função
+
+//registrar a função
+//function sayMyName() {
+ // document.write("Thiago")
+//}
+
+//executando a função
+//sayMyName()
+ 
+
+//condicionais
+
+/*
+
+const notaFinal = 7
+
+if (notaFinal < 5 ) {
+    document.write("Reprovado")
+} else {
+  document.write("Aprovado")
+}
+
+*/
+
+// REPETIÇÕES (LOOP)
+
+/*
+for (i = 0; i < 10; i++) {
+  document.write(`<p> ${i} </p>`)
+}
+ 
+
+*/
